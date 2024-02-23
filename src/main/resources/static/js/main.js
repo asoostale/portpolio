ab9ec1bbd4f2afe64c55a148d53f140fd993250c
SooYoung Joo <asoostale@naver.com>
2024-02-23 16:44:46 +0900

    arrow-up 효과 밥 먹고 다시 시작

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,10 +6,30 @@ document.addEventListener('scroll', () => {
     console.log(window.scrollY);
     if (window.scrollY > headerHeight) {
         header.classList.add('header--dark');
-    }else {
+    } else {
         header.classList.remove('header--dark');
     }
 })
+
+//Home 섹션을 아래로 스크롤 시 투명하게 처리 함.
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// ArrowUp버튼 아래로 스크롤 시 투명하게 처리
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
+
+});
+
+
 
 
 
