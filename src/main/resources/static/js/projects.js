b19653999e8c460a847dadc2af7a85b05453264a
SooYoung Joo <asoostale@gmail.com>
2024-02-23 22:18:16 +0900

    시안 완료

--- src/main/resources/static/js/projects.js
+++ src/main/resources/static/js/projects.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//프로젝트 필터링 관련 로직 처리
+// 프로젝트 필터링 관련 로직 처리
 const categories = document.querySelector('.categories');
 const projects = document.querySelectorAll('.project');
 const projectsContainer = document.querySelector('.projects');
@@ -10,22 +10,28 @@ categories.addEventListener('click', (event) => {
     if (filter == null) {
         return;
     }
-    //active 메뉴 재설정
+    // active 메뉴 재설정
+    handleActiveSelection(event.target);
+    // 프로젝트 필터링
+    filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
     const active = document.querySelector('.category--selected');
     active.classList.remove('category--selected');
-    event.target.classList.add('category--selected');
+    target.classList.add('category--selected');
+}
 
-
-    //프로젝트 필터링
+function filterProjects(filter) {
     projectsContainer.classList.add('anim-out');
     projects.forEach(project => {
         if (filter === 'all' || filter === project.dataset.type) {
-            project.style.display = 'block'; //block >> 기본값
+            project.style.display = 'block'; // block >> 기본값
         } else {
             project.style.display = 'none';
         }
     });
-    setTimeout(()=> {
+    setTimeout(() => {
         projectsContainer.classList.remove('anim-out');
-    }, 250)
-});
+    }, 250);
+}
